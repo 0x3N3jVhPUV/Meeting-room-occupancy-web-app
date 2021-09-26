ca6047964a5b07516d1c92e72ac8f7a0b65bed0a
andy <andyafene@hotmail.fr>
2021-09-26 06:04:00 +0200

    atInstant : Done

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from collections import Counter
 app = Flask(__name__)
 CORS(app)
 
-# app.config["DEBUG"] = True
+app.config["DEBUG"] = True
 client = MongoClient(
     "mongodb+srv://test:test@cluster0.vwlml.mongodb.net/?ssl=true&ssl_cert_reqs=CERT_NONE"
 )
@@ -26,6 +26,7 @@ def post_data():
     Ts = body["ts"]
     db["data"].insert_one({"sensor": sensor, "In": In, "Out": Out, "Ts": Ts})
     db["sensors"].insert_one({"sensor": sensor})
+    db["Ts"].insert_one({"Ts": Ts})
     return jsonify(
         {
             "status": "Data is posted to MongoDB!",
@@ -36,24 +37,38 @@ def post_data():
         }
     )
 
-# curl -X GET http://127.0.0.1:5000/api/occupancy
-@app.route("/api/occupancy", methods=["GET"])
-def make_sensor_list():
-    sensors_store = db["sensors"].find()
+
+def full_List_Maker(sensors_store, ts_store):
+    sensor_List = sensor_dict_maker(sensors_store)
+    ts_List = ts_dict_maker(ts_store)
+    Full_list = []
+    Full_list.append(sensor_List)
+    Full_list.append(ts_List)
+    return Full_list
+
+
+def sensor_dict_maker(data_store):
     dataJson = []
-    for data in sensors_store:
-        sensor = data["sensor"]
-        dataJson.append(sensor)
-    sensor_List = list(dict.fromkeys(dataJson))
-    return jsonify(sensor_List)
+    for data in data_store:
+        if data.get("sensor"):
+            sensor = data["sensor"]
+            dataJson.append(sensor)
+    data = list(dict.fromkeys(dataJson))
+    dataDict = {"sensors_list": data}
+    return dataDict
+    
+def ts_dict_maker(data_store):
+    dataJson = []
+    for data in data_store:
+        if data.get("Ts"):
+            Ts = data["Ts"]
+            dataJson.append(Ts)
+    data = list(dict.fromkeys(dataJson))
+    dataDict = {"ts_list": data}
+    return dataDict
 
 
-# curl --request GET http://127.0.0.1:5000/api/occupancy?sensor=XYZ
-@app.route("/api/occupancy/<sensor>", methods=["GET"])
-def get_data_by_sensor(sensor):
-    global sensor_name
-    sensor_name =  str(sensor)
-    Data_store = db["data"].find()
+def dataJsonMaker(Data_store):
     dataJson = []
     for data in Data_store:
         id = data["_id"]
@@ -63,18 +78,45 @@ def get_data_by_sensor(sensor):
         Ts = data["Ts"]
         dataDict = {"id": str(id), "sensor": sensor, "In": In, "Out": Out, "Ts": Ts}
         dataJson.append(dataDict)
+    return dataJson
 
+
+# curl -X GET 'http://127.0.0.1:5000/api/occupancy'
+# or
+# curl --request GET 'http://127.0.0.1:5000/api/occupancy?sensor=XYZ'
+# or
+# curl --request GET 'http://127.0.0.1:5000/api/occupancy?sensor=XYZ&atInstant=2018-11-14T14:00:00Z'
+@app.route("/api/occupancy", methods=["GET"])
+def occupancy():
+    sensor_name = request.args.get("sensor")
+    ts_value = request.args.get("atInstant")
+
+    data_store = db["data"].find()
+    sensors_store = db["sensors"].find()
+    ts_store = db["Ts"].find()
+
+    dataJson = dataJsonMaker(data_store)
     c = Counter()
-    for elem in dataJson:
-        if elem["sensor"] == sensor_name:
-            c.update(elem)
-    
-    inside = int(c.get('In')) - int(c.get('Out'))
-    Dict = {
-        "inside": inside,
-    }
-    print(Dict)
-    return jsonify(Dict)
+    Full_list = full_List_Maker(sensors_store, ts_store)
+
+    if sensor_name and ts_value:
+        for elem in dataJson:
+            if elem["sensor"] == sensor_name and elem["Ts"] == ts_value:
+                inside = int(elem.get("In")) - int(elem.get("Out"))
+                break
+        Dict = {"inside": inside}
+        Full_list.append(Dict)
+        return jsonify(Full_list)
+    elif sensor_name:
+        for elem in dataJson:
+            if elem["sensor"] == sensor_name:
+                c.update(elem)
+        inside = int(c.get("In")) - int(c.get("Out"))
+        Dict = {"inside": inside}
+        Full_list.append(Dict)
+        return jsonify(Full_list)
+    else:
+        return jsonify(Full_list)
 
 
 if __name__ == "__main__":
